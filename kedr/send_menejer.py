82d7b3e537d6cde24eca0bb84dfd10ae89bf636c
tuliman <57723817+tuliman@users.noreply.github.com>
2020-07-17 17:00:51 +0300

    Revert "Start project on Hotel Kedr" Добавил отзывы. This reverts commit e8ae75c3297f6ecb5d02c2e8821f0f3dccaa4468.

--- kedr/send_menejer.py
+++ kedr/send_menejer.py
@@ -15,7 +15,7 @@ def send_manager(body):
     server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     message = (', '.join(data for data in body.values()) + str(datetime.utcnow()))
     msg['From'] = EMAIL_ADDRESS
-    msg['To'] = EMAIL_ADDRESS
+    msg['To'] = 'kedrhaus_hotel@mail.ru'
     msg['Subject'] = "Me"
     msg.attach(MIMEText(message, 'plain', 'utf8'))
     server.sendmail(msg['From'], msg["To"], msg.as_string())
